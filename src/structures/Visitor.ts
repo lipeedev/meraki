81499de2eb46910ff649b31b848c98fb011a0bcd
lipeedev <anamariafakezaum@gmail.com>
2023-10-31 17:38:28 +0000

    refactor: organize module_access_field implementation

--- src/structures/Visitor.ts
+++ src/structures/Visitor.ts
@@ -7,46 +7,52 @@ type importModule = {
 }
 
 export class Visitor {
-    constructor(private astNodes: ASTNode[], private importModules?: importModule[]) { }
+  constructor(private astNodes: ASTNode[], private importModules?: importModule[]) { }
 
-    visit() {
-        for (const node of this.astNodes) {
+  private manageModuleAccessFieldFunctionCall(node: ASTNode, myModule: importModule) {
+    const functionInAst = this.astNodes.find(_node => _node.isFunctionCall && _node.functionCallValue?.name === (node.moduleAccessFieldValue?.field) as string);
+    const functionCall = myModule?.exports[node.moduleAccessFieldValue?.field as string];
 
-            //module Access Field
-            if (node.isModuleAccessField) {
-                const myModule = this.importModules?.find(mod => mod.name === node.moduleAccessFieldValue?.name);
-
-                if (!module) {
-                    sendError({
-                        message: `Module ${node.moduleAccessFieldValue?.name} not found`,
-                        line: node.line,
-                        column: node.column
-                    });
-                }
-
-                const isModuleAcessFieldFunctionCall = node.isFunctionCall && node.isModuleAccessField;
-
-                if (isModuleAcessFieldFunctionCall) {
-                    const functionInAst = this.astNodes.find(_node => _node.isFunctionCall && _node.functionCallValue?.name === (node.moduleAccessFieldValue?.field) as string);
-                    const functionCall = myModule?.exports[node.moduleAccessFieldValue?.field as string];
-
-                    if (!functionCall) {
-                        sendError({
-                            message: `Function "${node.moduleAccessFieldValue?.field}" not found in module "${node.moduleAccessFieldValue?.name}"`,
-                            line: node.line,
-                            column: node.column
-                        });
-                    }
-
-                    const args = functionInAst?.functionCallValue?.args.map(arg => arg.value);
-                    functionCall(...args as string[]);
-                }
-
-                //TODO: static property access
-
-            }
-
-            // TODO: Add more cases
-        }
+    if (!functionCall) {
+      sendError({
+        message: `Function "${node.moduleAccessFieldValue?.field}" not found in module "${node.moduleAccessFieldValue?.name}"`,
+        line: node.line,
+        column: node.column
+      });
     }
+
+    const args = functionInAst?.functionCallValue?.args;
+    functionCall(...args as any[]);
+  }
+
+  private manageModuleAccessField(node: ASTNode) {
+    const myModule = this.importModules?.find(mod => mod.name === node.moduleAccessFieldValue?.name);
+
+    if (!module) {
+      sendError({
+        message: `Module ${node.moduleAccessFieldValue?.name} not found`,
+        line: node.line,
+        column: node.column
+      });
+    }
+
+    const isModuleAcessFieldFunctionCall = node.isFunctionCall && node.isModuleAccessField;
+
+    if (isModuleAcessFieldFunctionCall) {
+      this.manageModuleAccessFieldFunctionCall(node, myModule as importModule);
+    }
+
+  }
+
+  public visit() {
+    for (const node of this.astNodes) {
+
+      //module Access Field
+      if (node.isModuleAccessField) {
+        this.manageModuleAccessField(node)
+      }
+
+      // TODO: Add more cases
+    }
+  }
 }
