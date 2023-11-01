2c56dde3fe844c81a01b26b0af182c871c99fc74
lipeedev <anamariafakezaum@gmail.com>
2023-11-01 14:12:38 +0000

    feat: add is_array_equals util function

---
+++ src/utils/isArrayEquals.ts
@@ -0,0 +1,5 @@
+import { Token } from "../structures/Token";
+
+export default function isArrayEquals(firstArray: Token[], secondArray: Token[]) {
+  return firstArray.every((item, index) => item.value === secondArray[index].value && item.type === secondArray[index].type)
+}
