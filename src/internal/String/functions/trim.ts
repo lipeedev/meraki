6a02f3edd35fae2ad1ec31639db0b8724760893e
whoisdon <conta.discordbaby@gmail.com>
2023-11-02 16:07:42 -0300

    Create trim.ts

---
+++ src/internal/String/functions/trim.ts
@@ -0,0 +1,25 @@
+import { TokenType } from "../../../structures/Token";
+import { ModuleFunctionCallParams } from "../../../structures/Visitor";
+import { sendError } from "../../../utils/sendError";
+
+const argsSize = 1;
+
+export function trim({ args, line, column }: ModuleFunctionCallParams) {
+  if (args.length !== argsSize) {
+    return sendError({
+      message: `trim() takes exactly ${argsSize} argument (${args.length} given)`,
+      line,
+      column
+    });
+  }
+
+  if (args[0].type !== TokenType.String) {
+    return sendError({
+      message: `trim() argument must be a string`,
+      line: args[0].line,
+      column: args[0].column
+    });
+  }
+
+  return { returnValue: args[0].value.trim() };
+}
