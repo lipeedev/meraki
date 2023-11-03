15a323c5ec056c619e839b190f182f7dfdd95489
whoisdon <conta.discordbaby@gmail.com>
2023-11-03 12:18:56 -0300

    feat: add variable return values logic to trim

---
+++ src/internal/String/functions/trim.ts
@@ -0,0 +1,56 @@
+import { TokenType } from "../../../structures/Token";
+import { ModuleFunctionCallParams } from "../../../structures/Visitor";
+import { sendError } from "../../../utils/sendError";
+
+const argsSize = 1;
+
+export function trim({ args, line, column, variables }: ModuleFunctionCallParams) {
+  let value;
+
+  if (args.length !== argsSize) {
+    return sendError({
+      message: `trim() takes exactly ${argsSize} argument (${args.length} given)`,
+      line,
+      column
+    })
+  }
+
+  if (args[0].type !== TokenType.String && args[0].type !== TokenType.Identifier) {
+    return sendError({
+      message: `trim() argument must be a string`,
+      line: args[0].line,
+      column: args[0].column
+    })
+  }
+
+  if (args[0].type === TokenType.Identifier) {
+    const variable = variables.find(_var => _var.name === args[0].value)
+    if (!variable) {
+      return sendError({
+        message: `Variable ${args[0].value} not found`,
+        line: args[0].line,
+        column: args[0].column
+      })
+    }
+
+    if (variable.type !== TokenType.String) {
+      return sendError({
+        message: `trim() argument must be a string`,
+        line: args[0].line,
+        column: args[0].column
+      })
+    }
+
+    value = variable.value;
+  }
+  else {
+    value = args[0].value;
+  }
+
+
+  return {
+    returnValue: value.trim(),
+    type: TokenType.String
+  }
+
+}
