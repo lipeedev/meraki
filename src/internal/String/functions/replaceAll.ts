d6318149884c919481818864cfbe3304436e81c6
whoisdon <conta.discordbaby@gmail.com>
2023-11-09 22:56:13 +0000

    feat: add replace and replaceAll functions and update the examples

---
+++ src/internal/String/functions/replaceAll.ts
@@ -0,0 +1,72 @@
+import { TokenType } from '../../../structures/Token';
+import { ModuleFunctionCallParams } from '../../../structures/Visitor';
+import { sendError } from '../../../utils/sendError';
+
+const argsSize = 3;
+
+export function replaceAll({ args, line, column, variables }: ModuleFunctionCallParams) {
+    if (args.length !== argsSize) {
+        return sendError({
+            message: `replaceAll() takes exactly ${argsSize} arguments (${args.length} given)`,
+            line,
+            column
+        });
+    }
+
+    if (args[0].type !== TokenType.String && args[0].type !== TokenType.Identifier) {
+        return sendError({
+            message: 'replaceAll() argument 1 must be a string',
+            line: args[0].line,
+            column: args[0].column
+        });
+    }
+
+    if (args[1].type !== TokenType.String && args[1].type !== TokenType.Identifier) {
+        return sendError({
+            message: 'replaceAll() argument 2 must be a string',
+            line: args[1].line,
+            column: args[1].column
+        });
+    }
+
+    if (args[2].type !== TokenType.String && args[2].type !== TokenType.Identifier) {
+        return sendError({
+            message: 'replaceAll() argument 3 must be a string',
+            line: args[2].line,
+            column: args[2].column
+        });
+    }
+
+    const string = args[0].type === TokenType.String ? args[0].value : variables.find(_var => _var.name === args[0].value)?.value;
+    const search = args[1].type === TokenType.String ? args[1].value : variables.find(_var => _var.name === args[1].value)?.value;
+    const replace = args[2].type === TokenType.String ? args[2].value : variables.find(_var => _var.name === args[2].value)?.value;
+
+    if (!string) {
+        return sendError({
+            message: `String "${args[0].value}" not found`,
+            line: args[0].line,
+            column: args[0].column
+        });
+    }
+
+    if (typeof search !== 'string') {
+        return sendError({
+            message: 'replaceAll() argument 2 must be a string',
+            line: args[1].line,
+            column: args[1].column
+        });
+    }
+
+    if (typeof replace !== 'string') {
+        return sendError({
+            message: 'replaceAll() argument 3 must be a string',
+            line: args[2].line,
+            column: args[2].column
+        });
+    }
+
+    return {
+        returnValue: string.replaceAll(search, replace),
+        type: TokenType.String
+    };
+}
